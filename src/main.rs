4bfe6e1efa3d2f507038c5c8ab1c1654fd91e94b
YahiroRyo <honakafn@gmail.com>
2022-01-03 13:46:06 +0900

    数値を表示できるようにした

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,20 @@
+mod strlib;
+mod middleware;
+
+use std::env;
+use strlib::code::Code;
+use strlib::strl::strtoi;
+use middleware::filter::middleware;
+
 fn main() {
+	let args: Vec<String> = env::args().collect();
+	middleware(args.clone());
+	let mut code: Code = Code::new(args[1].chars().collect());
+
+	println!(".intel_syntax noprefix");
+	println!(".globl main");
+	println!("main:");
+
+	println!("  mov rax, {}", strtoi(&mut code));
+	println!("  ret");
 }
